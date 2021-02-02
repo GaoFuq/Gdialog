1c0210740941777ee69d9ae64bad14e6279fef61
gaofuqiang <gaofuqiang>
2021-02-02 15:49:10 +0800

    .。。

--- wheelview/src/main/java/com/contrarywind/view/WheelView.java
+++ wheelview/src/main/java/com/contrarywind/view/WheelView.java
@@ -141,7 +141,7 @@ public class WheelView extends View {
     public WheelView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        textSize = getResources().getDimensionPixelSize(R.dimen.pickerview_textsize);//默认大小
+        textSize = getResources().getDimensionPixelSize(R.dimen.textsize);//默认大小
 
         DisplayMetrics dm = getResources().getDisplayMetrics();
         float density = dm.density; // 屏幕密度比（0.75/1.0/1.5/2.0/3.0）
